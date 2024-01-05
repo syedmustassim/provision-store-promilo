3f2123073b6e626376d59744d81c133acb388669
Syed Mustassim <smustassim@gmail.com>
2024-01-05 20:54:05 +0530

    feat: basic folder structure setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return(
+        <div>
+            <h1>Navbar component</h1>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/product-list/ProductList.jsx
@@ -0,0 +1,9 @@
+const ProductList = () => {
+    return(
+        <div>
+            <h1>Product listing page</h1>
+        </div>
+    )
+}
+
+export default ProductList;
